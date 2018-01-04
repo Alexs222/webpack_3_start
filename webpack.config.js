a2420aacc6f7b8219dc954ad73730f9408600bd8
alexs222 <aleks.nikulin@gmail.com>
2018-01-04 20:04:30 +0200

    add pug

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,25 @@ const PATHS = {
 };
 
 module.exports = {
-        entry: PATHS.source + '/index.js',
-        output: {
-                path: PATHS.build,
-                filename: '[name].js'
-        },
-        plugins: [
-                new HtmlWebpackPlugin({
-                        title: 'Webpack app'
-                })
-        ]
+    entry: PATHS.source + '/index.js',
+    output: {
+            path: PATHS.build,
+            filename: '[name].js'
+    },
+    plugins: [
+            new HtmlWebpackPlugin({
+            template: PATHS.source + '/index.pug'
+            })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        ]
+    }
 };
