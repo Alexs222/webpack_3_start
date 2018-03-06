a541673ed17fa4abc41b37d88549bfb18ddc3c74
alexs222 <aleks.nikulin@gmail.com>
2018-03-06 23:32:52 +0200

    test styles

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -13,3 +13,32 @@ console.log(jQuery);
 import test from '../../components/test'
 
 test('Привет webpack')
+
+
+    
+    // var goTimer = document.getElementById("go");
+    // var stopTimer = document.getElementById("stop");
+    // var displeyTimer = document.getElementsByClassName("timer");
+    // var tested = document.getElementsByClassName("tested");
+
+
+    // var p = document.createElement("p");
+    // var node = document.createTextNode("Новый заголовок");
+    // p.appendChild(node);
+    // console.log(p);
+    // var wrapper = document.getElementsByClassName("wrapper");
+    // document.getElementsByClassName("wrapper").appendChild(p);
+    // console.log(wrapper);
+    // wrapper.replaceChild(p, tested);
+    // wrapper.appendChild(p);
+
+var div = document.createElement('div');
+div.className = "newdiv";
+div.innerHTML = "Новый текст через innerHTML";
+console.log(div);
+
+
+// wrapper.appendChild(div);
+// wrapper.insertBefore(div, wrapper.firstChild);
+wrapper.replaceChild(div, tested);
+
